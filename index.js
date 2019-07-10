7aa4b68f8d0ebde5f5298879782243b52b378c84
Arpit Vasani <vasani.arpit@gmail.com>
2019-07-10 19:27:43 +0530

    CSS changes and other social media icons

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var line = new LeaderLine(
     document.querySelector("#left-instagram"),
     document.querySelector(".dashboard"),
     {
-        dash: { len: 15, gap: 25 },
+        dash: { len: 4, gap: 25, animation: { duration: 200 } },
         middleLabel: LeaderLine.pathLabel({ text: `This is additional label` }),
         endPlug: "arrow2"
     });
+
+var postReceived = () => {
+    line.setOptions({ dash: { len: 300, gap: 900, animation: { duration: 2000 } } });
+    setTimeout(() => {
+        line.setOptions({ dash: { len: 4, gap: 25, animation: { duration: 200 } } });
+    }, 1500);
+}
